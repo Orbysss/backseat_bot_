83d04c4638c2710fcfaf85b8e1a5bb2eb3b7e520
Brandon Pessman <34040658+BrandonPessman@users.noreply.github.com>
2021-05-08 04:29:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let hintsDestroyed = 0;
 let startTime = new Date();
 
 // Channels to add to
-let channelList = ['CryBabyCarly', 'Nerdware', 'rebeccaloran']
+let channelList = ['CryBabyCarly', 'Nerdware', 'rebeccaloran', 'fleoren']
 
 // Spaces on a Chess Board
 const spaces = [
