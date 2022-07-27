f12719614fba8ac8f32b906186c7cf27fb5c8783
Orbysss <109703725+Orbysss@users.noreply.github.com>
2022-07-27 18:21:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -128,8 +128,6 @@ client.on('chat', (channel, user, message, self) => {
 
                 const minute = 1000 * 60;
                 const hour = minute * 60;
-                const day = hour * 24;
-                const year = day * 365;
 
                 const d = new Date();
                 let years = Math.round(d.getTime() / year);
