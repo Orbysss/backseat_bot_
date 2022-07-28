d52c6cf8ddc4ee7ccddff4f635e05b092376f5de
Orbysss <109703725+Orbysss@users.noreply.github.com>
2022-07-28 01:13:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ client.on('chat', (channel, user, message, self) => {
             if ((user.mod || user.username == channel.slice(1, channel.length)) && message.includes ("!so")) {
                  const shout = message.slice(1).split('@');
                  console.log(shout);
+                 shout.shift();
                  client.action(channel.slice(1, channel.length), `	▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ Check out ${shout.join(' ')} and give them a follow at twitch.tv/${shout.join(' ')} 	▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ `);    
             }
     
